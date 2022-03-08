a3332b95912fbbae05506af4af4ae0cc74655503
Erdi Sener <erdi.sener@sisu9.com>
2022-03-08 23:24:13 +0300

    fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -100,7 +100,7 @@ let resumeButton = document.getElementById('resume-button')
 // Html2pdf options
 let opt = {
   margin:       0,
-  filename:     'kayhan_CV.pdf',
+  filename:     'erdi_sener_cv.pdf',
   image:        { type: 'jpeg', quality: 0.98 },
   html2canvas:  { scale: 4 },
   jsPDF:        { format: 'a4', orientation: 'portrait' }
